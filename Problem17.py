eedc5b34942034489f6d34c23b431cd74b69bbf8
frmeu9 <franrogowski@gmail.com>
2018-06-18 13:08:44 -0400

    update 17

--- Problem17.py
+++ Problem17.py
@@ -7,3 +7,14 @@
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters
 # and 115 (one hundred and fifteen) contains 20 letters.
 # The use of "and" when writing out numbers is in compliance with British usage.
+
+import numpy as np
+
+numList = np.linspace(1, 1000, num=1000)
+numList_str = []
+
+for num in range(len(numList)):
+    numList_str.append(str(numList[num]))
+    numList_str[num] = numList_str[num].rstrip('.0')
+
+numList_str[999] = '1000'
